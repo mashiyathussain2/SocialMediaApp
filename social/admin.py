c3cac9050b8972fb900c6d1643c941e7b65deb30
Mashiyat Hussain <39239687+mashiyathussain2@users.noreply.github.com>
2021-06-25 02:25:14 +0530

    Add files via upload

---
+++ social/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from social import models
+
+# Register your models here.
+admin.site.register([
+    models.Post,
+    models.Friends,
+    models.Like,
+    models.Comment
+])
